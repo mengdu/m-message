8c0d73ba3685d2f6b2d02cf9db6f054e7205784e
v_yguiliu <v_yguiliu@tencent.com>
2019-04-03 19:59:03 +0800

    feat: 增加 svg icon 组件

---
+++ src/icon/icon.js
@@ -0,0 +1,51 @@
+'use strict'
+const icons = {}
+
+export const addIcon = function (name, options) {
+  icons[name] = options
+}
+
+const Icon = {
+  name: 'Icon',
+  props: {
+    name: String,
+    color: String
+  },
+  computed: {
+    svg () {
+      return icons[this.name]
+    }
+  },
+  render (h) {
+    const svg = this.svg
+
+    if (!svg) {
+      console.warn(`The name of '${this.name}' could not be found.`)
+      return <span class="m-svg-icon"></span>
+    }
+
+    const style = {
+      color: this.color ? this.color : (svg.fill ? svg.fill : null)
+    }
+
+    return (
+      <span class={['m-svg-icon', 'm-svg-icon--' + this.name]}>
+        <svg
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox={svg.viewBox}
+          style={style}
+          class={svg.class}>
+          { svg.defs && <defs domPropsInnerHTML={svg.defs}></defs> }
+          { svg.path && <path fill="currentColor" d={svg.path} /> }
+          { svg.html && <g domPropsInnerHTML={svg.html}></g> }
+          {this.$slots.default}
+        </svg>
+      </span>
+    )
+  }
+}
+
+Icon.add = addIcon
+
+export default Icon
---
+++ src/icon/index.js
@@ -0,0 +1,14 @@
+import Icon from './icon'
+import InfoIcon from './svgs/info'
+import SuccessIcon from './svgs/success'
+import ErrorIcon from './svgs/error'
+import WarningIcon from './svgs/warning'
+import LoadingIcon from './svgs/loading'
+
+Icon.add(InfoIcon.name, InfoIcon)
+Icon.add(SuccessIcon.name, SuccessIcon)
+Icon.add(ErrorIcon.name, ErrorIcon)
+Icon.add(WarningIcon.name, WarningIcon)
+Icon.add(LoadingIcon.name, LoadingIcon)
+
+export default Icon

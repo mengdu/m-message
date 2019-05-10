50e9ddf9ee27e07ee40e44bbaf53f7a9a0c15f65
mengdu <1752295326@qq.com>
2019-05-10 23:58:29 +0800

    feat: 优化 message 组件

--- src/message.vue
+++ src/message.vue
@@ -1,21 +1,29 @@
 <template>
-  <div class="m-message">
-    <div class="m-message--icon">
-      <img v-if="iconImg" :src="iconImg" class="m-message-icon-img"/>
+  <div class="m-message" v-show="visible">
+    <div class="m-message--icon" v-if="iconImg || $slots.icon">
+      <img :src="iconImg" v-if="iconImg" class="m-message-icon-img"/>
       <slot name="icon" v-else></slot>
     </div>
-    <div class="m-message--content">
-      <div class="m-message--title" v-if="title">{{ title }}</div>
-      {{content}}
+    <div class="m-message-content">
+      <div class="m-message--title" v-if="title || $slots.title"><slot name="title">{{ title }}</slot></div>
+      <div class="m-message--description" v-if="!collapsed"><slot>{{ content }}</slot></div>
     </div>
     <div class="m-message--control">
-      <button class="m-message--button m-message--arrow-dowm">
-        <svg viewBox="0 0 35 35" width="19" height="19" version="1.1" fill="currentColor">
+      <slot name="button"></slot>
+      <button class="m-message--button m-message--arrow-down"
+        v-if="title || $slots.title"
+        :class="{
+          'is-collapsed': collapsed
+        }"
+        @click="triggerCollapse">
+        <svg viewBox="0 0 35 35" width="20" height="20" version="1.1" fill="currentColor">
             <path class="st0" d="M9.4,13.9c-0.2,0.2-0.2,0.6,0,0.8l8.1,8.1l0,0l0,0l8.1-8.1c0.2-0.2,0.2-0.6,0-0.8l-1.3-1.3 c-0.2-0.2-0.6-0.2-0.8,0l-5.5,5.5c-0.2,0.2-0.6,0.2-0.8,0l-5.5-5.5c-0.2-0.2-0.6-0.2-0.8,0L9.4,13.9z"/>
           </svg>
       </button>
-      <button class="m-message--button m-message--close" @click="handleClose" v-if="canClose">
-        <svg viewBox="0 0 35 35" width="19" height="19" version="1.1" fill="currentColor">
+      <button class="m-message--button m-message--close"
+        v-if="closable"
+        @click="handleClose">
+        <svg viewBox="0 0 35 35" width="20" height="20" version="1.1" fill="currentColor">
           <path d="M19.5,17.5l5.1,5.1l-2,2l-5.1-5.1l-5.1,5.1l-2-2l5.1-5.1l-5.1-5.1l2-2l5.1,5.1l5.1-5.1l2,2L19.5,17.5z"></path>
         </svg>
       </button>
@@ -25,16 +33,34 @@
 
 <script>
 export default {
+  name: 'm-message',
   props: {
     iconImg: String,
-    canClose: Boolean,
+    closable: Boolean,
     closeHandler: Function,
     title: String,
-    content: String
+    content: String,
+    isCollapsed: {
+      type: Boolean,
+      default: true
+    }
+  },
+  data () {
+    return {
+      visible: true,
+      collapsed: this.isCollapsed
+    }
   },
   methods: {
+    triggerCollapse () {
+      this.collapsed = !this.collapsed
+    },
+    close () {
+      this.visible = false
+    },
     handleClose () {
-      if (typeof this.closeHandler === 'function') this.closeHandler()
+      if (typeof this.closeHandler === 'function') this.closeHandler(this.close)
+      else this.close()
     }
   }
 }

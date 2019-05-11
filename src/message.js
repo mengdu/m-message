b145960a4295268b7aab90e76fab72ce5b9e3fbb
mengdu <1752295326@qq.com>
2019-05-11 23:24:15 +0800

    feat: 增加 options.position, options.hasMask 属性支持

--- src/message.js
+++ src/message.js
@@ -4,18 +4,29 @@ const MessageConstructor = Vue.extend(MessageTemplate)
 
 const instances = []
 let count = 1
-let msgContainer = null
+const containers = {}
 
 const Message = function (options) {
   let userOnClose = options.onClose
-  if (!msgContainer) {
-    msgContainer = document.createElement('div')
-    msgContainer.className = 'm-message-container'
-    document.body.appendChild(msgContainer)
+  const position = options.position || 'top-center'
+  const hasMask = options.hasMask || false
+  const containerKey = position + (hasMask ? '-mask' : '')
+  let containerEl = containers[containerKey]
+
+  if (!containerEl) {
+    containerEl = containers[containerKey] = document.createElement('div')
+    containerEl.className = [
+      'm-message-container',
+      'is-' + position,
+      hasMask ? 'has-mask' : ''
+    ].filter(function (e) { return !!e }).join(' ')
+    document.body.appendChild(containerEl)
   }
+
   if (options.zIndex) {
-    msgContainer.style.zIndex = options.zIndex
+    containerEl.style.zIndex = options.zIndex
   }
+
   const id = count++
   options.onClose = function () {
     Message.close(id, userOnClose)
@@ -27,7 +38,14 @@ const Message = function (options) {
   })
 
   instance.id = id
-  msgContainer.appendChild(instance.$el)
+  instance.containerKey = containerKey
+
+  // for position of bottom-*
+  if (position.indexOf('bottom') === 0 && containerEl.firstChild) {
+    containerEl.insertBefore(instance.$el, containerEl.firstChild)
+  } else {
+    containerEl.appendChild(instance.$el)
+  }
   // 挂载后再设置显示才有过渡效果
   instance.show = true
 
@@ -39,18 +57,37 @@ const Message = function (options) {
 Message.close = function (id, userOnClose) {
   for (let i = 0, len = instances.length; i < len; i++) {
     if (id === instances[i].id) {
+      const { containerKey, hasMask } = instances[i]
+      // 响应options.onClose
       if (typeof userOnClose === 'function') {
         userOnClose(instances[i])
       }
       instances[i] = null
       instances.splice(i, 1)
+
+      const index = instances.findIndex(function (e) {
+        return e.containerKey === containerKey
+      })
+      // 如果开启遮罩，300ms 后移除容器（不移除白屏时间太长）
+      if (hasMask && index === -1) {
+        setTimeout(function () {
+          containers[containerKey].remove()
+          containers[containerKey] = null
+        }, 300)
+      }
       break
     }
   }
+
   setTimeout(function () {
-    if (instances.length === 0 && msgContainer) {
-      msgContainer.remove()
-      msgContainer = null
+    // 当前没有消息后，移除容器
+    if (instances.length === 0) {
+      for (let type in containers) {
+        if (containers[type]) {
+          containers[type].remove()
+          containers[type] = null
+        }
+      }
     }
   }, 3000)
 }
@@ -61,12 +98,12 @@ Message.closeAll = function () {
   }
 }
 
-const types = ['info', 'success', 'error', 'warning', 'loading']
+const types = [ 'info', 'success', 'error', 'warning', 'loading' ]
 
 types.forEach(type => {
   Message[type] = function (options) {
-    options = typeof options === 'string' ? {message: options} : options
-    return Message({...options, type})
+    options = typeof options === 'string' ? { message: options } : options
+    return Message({ ...options, type })
   }
 })
 
